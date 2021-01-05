fd57e1c30fd36e0f37cac49e2e6326bc5f3de28a
Evan Hu (YiFan Hu) <eiahb3838ya@gmail.com>
2021-01-05 17:17:39 +0800

    1.5

--- GeneticPogramming/old_version/factorEval.py
+++ GeneticPogramming/old_version/factorEval.py
@@ -8,17 +8,30 @@ Created on Fri Dec 25 13:35:48 2020
 # from functools import partial
 import numpy as np
 from copy import copy
-from Tool.GeneralData import GeneralData
-from GeneticPogramming.utils import rowwise_corrcoef, get_residual   
 
+from GeneticPogramming.utils import rowwise_corrcoef, get_residual
 
+try:
+    globalVars.logger.info('load factorEval')
+except:
+    from Tool import globalVars
+    globalVars.logger.info('load factorEval')
+    
+
+try:
+    barraStack = np.stack([aB.generalData for aB in globalVars.barra.values()],axis = 2)
+except:
+    from GetData import load_all
+    globalVars.logger.warning('factorEval is loading things')
+    load_all()
+    barraStack = np.stack([aB.generalData for aB in globalVars.barra.values()],axis = 2)
 
 # evaluate function 评价函数
-def ic_evaluator(factor : GeneralData, pctChange:GeneralData) -> float:    
-    ic = rowwise_corrcoef(factor, pctChange.get_shifted(-1))
+def ic_evaluator(factor):    
+    ic = rowwise_corrcoef(factor, globalVars.materialData['pctChange'].get_shifted(-1))
     return(ic)
 
-def residual_preprocess(factor, toRegStack):
+def residual_preprocess(factor, toRegStack = barraStack):
     assert toRegStack.shape[:2] == factor.generalData.shape, 'make sure the factor shape is same as the risk factors'
     residualStack = np.full_like(factor.generalData, np.nan)
     thetaStack = np.full((toRegStack.shape[0],toRegStack.shape[2]), np.nan)
@@ -33,13 +46,5 @@ def residual_preprocess(factor, toRegStack):
     return(outFactor)
     
 if __name__  ==  '__main__':
-    from Tool import globalVars
-    try:
-        barraStack = np.stack([aB.generalData for aB in globalVars.barra.values()],axis = 2)
-    except:
-        from GetData import load_all
-        globalVars.logger.warning('factorEval is loading things')
-        load_all()
-        barraStack = np.stack([aB.generalData for aB in globalVars.barra.values()],axis = 2)
     factor = globalVars.materialData['open']
     residual_preprocess(factor, barraStack)
